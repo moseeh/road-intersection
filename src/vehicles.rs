1218359aeb66f170be9af628b9ce9930623f90cc
anne-okingo <annekadiso@gmail.com>
2025-04-21 22:30:41 +0300

    feat: implement a random routr generator function that generates a random route (Left, Right, or Straight)

--- src/vehicles.rs
+++ src/vehicles.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Direction {
@@ -12,4 +13,15 @@ pub enum Route {
     Left,
     Right,
     Straight,
+}
+
+impl Route {
+    pub fn random() -> Self {
+        let mut rng = rand::thread_rng();
+        match rng.gen_range(0..3) {
+            0 => Route::Left,
+            1 => Route::Right,
+            _ => Route::Straight,
+        }
+    }
 }
