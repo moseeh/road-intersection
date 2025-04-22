15dfcdab3fc02e95727dafefad7f74078c3cfab1
moonyango <mosesadrian825@gmail.com>
2025-04-22 16:29:25 +0300

    feat: traffic lights implementation

---
+++ src/traffic_light.rs
@@ -0,0 +1,41 @@
+use rand::Rng;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum LightState {
+    Red,
+    Green,
+}
+#[allow(dead_code)]
+pub struct TrafficLight {
+    rect: Rect,
+    state: LightState,
+}
+
+impl TrafficLight {
+    pub fn new(x: i32, y: i32, w: u32, h: u32) -> Self {
+        TrafficLight {
+            rect: Rect::new(x, y, w, h),
+            state: LightState::Red,
+        }
+    }
+    pub fn update(&mut self) {
+        let mut rng = rand::rng();
+        self.state = if rng.random_bool(0.5) {
+            LightState::Green
+        } else {
+            LightState::Red
+        };
+    }
+    pub fn draw(&self, canvas: &mut Canvas<Window>) {
+        let color = match self.state {
+            LightState::Red => Color::RGB(200, 0, 0),
+            LightState::Green => Color::RGB(0, 200, 0),
+        };
+
+        canvas.set_draw_color(color);
+        let _ = canvas.fill_rect(self.rect);
+    }
+}
